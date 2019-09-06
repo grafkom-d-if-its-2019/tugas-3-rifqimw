38d8ef7b868af70a661b83d5110dd852936472bf
Hadziq 哈明飛 <hadziq@gmail.com>
2019-09-06 10:28:17 +0700

    Bisa pakai file GLSL

---
+++ fragment.glsl
@@ -0,0 +1,3 @@
+void main() {
+  gl_FragColor = vec4(0.3216, 0.8549, 0.8549, 1.0);
+}
--- libs/glUtils.js
+++ libs/glUtils.js
@@ -1,6 +1,6 @@
 (function(global) {
   var glUtils = {
-    VERSION: '0.0.1',
+    VERSION: '0.0.3',
     checkWebGL: function(canvas) {
       var gl;
       var contexts =["webgl", "moz-webgl", "webkit-3d", "experimental-webgl"];
@@ -43,7 +43,102 @@
         return null;
       }
       return program;
+    },
+    SL: {
+      /*
+       * Ajax stuff
+       */
+      XMLHttpFactories: [
+        function () { return new XMLHttpRequest() },
+        function () { return new ActiveXObject("Msxml2.XMLHTTP") },
+        function () { return new ActiveXObject("Msxml3.XMLHTTP") },
+        function () { return new ActiveXObject("Microsoft.XMLHTTP") }
+      ],
+
+      createXMLHTTPObject: function() {
+        
+        var xmlhttp = false;
+        
+        for (var i = 0; i < this.XMLHttpFactories.length; i++) {
+          
+          try {
+            xmlhttp = this.XMLHttpFactories[i]();
+          } catch (error) {
+            continue;
+          }
+          break;
+        }
+
+        return xmlhttp;
+      },
+      
+      sendRequest: function(url, callback, element) {
+        
+        var req = this.createXMLHTTPObject();
+        
+        if (!req) return;
+        var method = "GET";
+        req.open(method, url, true);
+        
+        req.onreadystatechange = function() {
+          
+          if (req.readyState != 4) return;
+          if (req.status != 0 && req.status != 200 && req.status != 304) {
+            return;
+          }
+          callback(req, element);
+        }
+        
+        if (req.readyState == 4) return;
+        req.send();
+      },
+      /*
+       * Signals
+       */
+      init: function(options) {
+        
+        var options = options || {};
+        this.callback = options.callback || function() {};
+        this.elemName = options.elemName || "shader";
+        this.dataSrc = options.dataSrc || "data-src";
+        this.dataType = options.dataType || "data-type";
+        this.dataVersion = options.dataVersion || "data-version";
+        this.shaderElems = document.getElementsByName(this.elemName);
+        this.loadedSignal = new global.signals.Signal();
+        this.Shaders = this.Shaders || {};
+        this.loadedSignal.add(this.callback);
+        this.slShaderCount = this.shaderElems.length;
+        
+        for(var i = 0; i < this.slShaderCount; i++) {
+          var shader = this.shaderElems[i];
+          this.sendRequest(shader.getAttribute(this.dataSrc), this.processShader, shader);
+        }
+        
+        this.checkForComplete();
+      },
+      
+      checkForComplete: function() {
+        if(!this.slShaderCount) {
+          this.loadedSignal.dispatch();
+        }
+      },
+      
+      processShader: function(req, element) {
+        
+        glUtils.SL.slShaderCount--;
+        var version = element.getAttribute(glUtils.SL.dataVersion);
+        
+        if(!glUtils.SL.Shaders[version]) {
+          glUtils.SL.Shaders[version] = {
+            vertex: '',
+            fragment: ''
+          };
+        }
+        console.log("\nShader source:\n"+ req.responseText);
+        glUtils.SL.Shaders[version][element.getAttribute(glUtils.SL.dataType)] = req.responseText;
+        glUtils.SL.checkForComplete();
+      }
     }
-  }
+  };
   global.glUtils = glUtils;
 })(window || this);
